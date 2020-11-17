ebfdcc59fb5676ead2d4c70ecb5f0d777c6a7398
Sahithyan <sahithyan2701@gmail.com>
2020-11-17 17:07:49 +0530

    configured browser builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,30 @@
-const path = require('path');
+const path = require("path")
 
-module.exports = {
-	entry: "./src/index.ts",
-	module: {
-		rules: [
-			{
-				test: /\.ts/,
-				exclude: /node_modules/,
-				use: 'babel-loader'
-			}
-		]
-	},
-	resolve: {
-		extensions: ['.ts']
-	},
-	target: 'node',
-	output: {
-		library: {
-			name: 'JokeAPI',
-			type: 'umd'
+module.exports = (env) => {
+	let target = env.TARGET
+
+	return {
+		entry: "./src/index.ts",
+		module: {
+			rules: [
+				{
+					test: /\.ts/,
+					exclude: /node_modules/,
+					use: "babel-loader"
+				}
+			]
 		},
-		filename: 'index.js',
-		path: path.join(__dirname, 'dist')
+		target,
+		resolve: {
+			extensions: [".ts"]
+		},
+		output: {
+			library: {
+				name: "JokeAPI",
+				type: "umd"
+			},
+			filename: "index.js",
+			path: path.join(__dirname, "dist-" + target)
+		}
 	}
 }
